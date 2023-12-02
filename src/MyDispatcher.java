3e3c9a8de190dc91f1347b54e7cfce780a894cef
Matei Ionescu <matei.ionescu7@gmail.com>
2023-12-02 22:53:47 +0200

    add solution 120/120p

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -1,5 +1,7 @@
 /* Implement this class. */
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MyDispatcher extends Dispatcher {
@@ -7,8 +9,41 @@ public class MyDispatcher extends Dispatcher {
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
     }
+    int round_robin_index = 0;
 
     @Override
     public void addTask(Task task) {
+        switch(algorithm) {
+            case ROUND_ROBIN -> addRoundRobinTask(task);
+            case SHORTEST_QUEUE -> addShortestQueueTask(task);
+            case SIZE_INTERVAL_TASK_ASSIGNMENT -> addSizeIntervalTask(task);
+            case LEAST_WORK_LEFT -> addLeastWorkLeft(task);
+        }
+    }
+
+    private void addRoundRobinTask(Task task) {
+        // send the task to the host, based on the index of the last host a task was sent to
+        hosts.get(++round_robin_index % hosts.size()).addTask(task);
+    }
+
+    private void addShortestQueueTask(Task task) {
+        // send the task to the host with the shortest queue
+        Host designated_host = Collections.min(hosts, Comparator.comparingInt(Host::getQueueSize));
+        designated_host.addTask(task);
+    }
+
+    private void addSizeIntervalTask(Task task) {
+        // send the task to the host based on the task type
+        switch (task.getType()) {
+            case SHORT -> hosts.get(0).addTask(task);
+            case MEDIUM -> hosts.get(1).addTask(task);
+            case LONG -> hosts.get(2).addTask(task);
+        }
+    }
+
+    private void addLeastWorkLeft(Task task) {
+        // send the task to the host based on the work left
+        Host designated_host = Collections.min(hosts, Comparator.comparingLong(Host::getWorkLeft));
+        designated_host.addTask(task);
     }
 }
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,25 +1,109 @@
 /* Implement this class. */
 
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class MyHost extends Host {
+    private final PriorityBlockingQueue<Task> queued_tasks = new PriorityBlockingQueue<>(10,
+            Comparator.comparingInt(Task::getPriority).reversed().thenComparingInt(Task::getStart));
+    private static final Task POISON_VALUE = new Task(-1, -1, -1, TaskType.SHORT, -1, false);
+    private Task running_task;
+    private final AtomicLong work_left = new AtomicLong();
+    private boolean exit = false;
+
+    private void executeTask() {
+        long start_sleep_time = 0;
+        long time_left = running_task.getLeft();
+
+        try {
+            // sleep for one second at a time in order to get a more accurate work_left value
+            while (time_left > 0) {
+                // sleep for one second if time_left if greater than one second or for time_left milliseconds otherwise
+                long sleeping_time = (time_left < 1000) ? time_left : 1000;
+
+                // get the current time in case there will be an interrupt and sleep will not finish
+                start_sleep_time = System.currentTimeMillis();
+                sleep(sleeping_time);
+
+                // update the time_left on the current task and the work_left on all tasks
+                time_left -= sleeping_time;
+                work_left.addAndGet(-sleeping_time);
+            }
+
+            // finish the task
+            running_task.finish();
+            running_task = null;
+
+        } catch (InterruptedException e) {
+            // task was interrupted
+
+            // sleeping time is the time that the task was running for before being interrupted
+            long end_sleep_time = System.currentTimeMillis();
+            long sleeping_time = end_sleep_time - start_sleep_time;
+
+            // update the work_left and the time left on the running task
+            work_left.addAndGet(-sleeping_time);
+            running_task.setLeft(time_left - sleeping_time);
+
+            // add the task back to the queue
+            queued_tasks.add(running_task);
+            running_task = null;
+        }
+    }
+
     @Override
     public void run() {
+        while (!exit) {
+            try {
+                // get the next task from the priority queue. If there is no task in the queue, it will block and wait
+                // for one
+                running_task = queued_tasks.take();
+
+                // if the poison value is received, end the execution of the thread
+                if (running_task == POISON_VALUE) {
+                    exit = true;
+
+                // otherwise, execute the task
+                } else {
+                    executeTask();
+                }
+
+            } catch (InterruptedException e) {
+                queued_tasks.add(POISON_VALUE);
+            }
+        }
     }
 
     @Override
     public void addTask(Task task) {
+        // add the task to the bocking priority queue
+        queued_tasks.add(task);
+
+        // update work left atomically
+        work_left.addAndGet(task.getDuration());
+
+        // if the running task is preemptible and a task with a higher priority was added to the queue, interrupt the
+        // running task
+        if (running_task != null && running_task.isPreemptible() && running_task.getPriority() < task.getPriority()) {
+            interrupt();
+        }
     }
 
     @Override
     public int getQueueSize() {
-        return 0;
+        // return the size of the queue, including the running task if there is one
+        return queued_tasks.size() + ((running_task != null) ? 1 : 0);
     }
 
     @Override
     public long getWorkLeft() {
-        return 0;
+        return work_left.longValue();
     }
 
     @Override
     public void shutdown() {
+        // add a value to the queue that will end the execution of the thread
+        queued_tasks.add(POISON_VALUE);
     }
 }
